cf49980215f4a304d3a27f609f542a608a9f8065
atharva101010 <patilatharva260@gmail.com>
2026-08-31 17:57:12 +0530

    Updated code

--- OOP.py
+++ OOP.py
@@ -31,7 +31,7 @@ print(o3.a,o3.b)  '''
 
 
 
-class Bank:
+'''class Bank:
     bname='SBI'
     loc='Mumbai'
 c1=Bank()
@@ -54,4 +54,77 @@ c1.loc='Mumbai' #Syntax : objname.propertyname=new_value
 print('After Modification in Object : ')
 print(Bank.bname,Bank.loc)
 print(c1.bname,c1.loc)
-print(c2.bname,c2.loc)
+print(c2.bname,c2.loc) '''
+
+
+
+
+
+#  __init__ Method
+'''
+Constructor/__inti__/initialisation method:
+1) It is a method which is used to initialize the members of object.
+2) Name should be __init__ .
+3) We can pass arguments either in function call/in class call/in object creation only it there exists __inti__ method .
+4) According to industrial standard rule we need to use "Self" to store the address of object.
+5) And "Self" should be the 1st argument
+'''
+
+#Syntax:
+'''     class cname:
+
+
+         def __init__(Self,var1,var2,var3,.....varn):
+             self.var1=var1
+             self.var2=var2
+             self.var3=var3
+
+
+
+             self.varn=varn
+obj=cname(val1,val2,val3,......valn)    '''
+
+'''Create a class called Bank with 4 class members, 10 objects consists of 7\
+objects members each.
+
+class Bank:
+    bname='ICICI'
+    loc='Mumbai'
+    helpline=801234
+    website='www.icici.com'
+
+    def __init__(self,name,phno,addr,email): # __init__ acts as a constructor
+        self.name=name
+        self.phno=phno
+        self.addr=addr
+        self.email=email
+
+#now to declare values for c1
+c1=Bank('A',9876543210,'Marg','a@gmail.com')
+
+#now to declare values for c2
+c2=Bank('B',98745632120,'Mandir','b@gmail.com')
+
+#now to declare values for c3
+c3=Bank('C',686748096,'Bapat','c@gmail.com')
+
+print(c1.name,c1.phno,c1.addr,c1.email)
+print(c2.name,c2.phno,c2.addr,c2.email)
+print(c3.name,c3.phno,c3.addr,c3.email) '''
+
+
+'''Program to create a class called "Company" with 3 class members and 1 object with 4 object members
+
+class Company:
+    cname='TCS'
+    loc='Mumbai'
+    CEO='Kritivasan'
+    def __init__(self,empname,eid,sal,desig):
+        self.empname=empname
+        self.eid=eid
+        self.sal=sal
+        self.desig=desig
+
+e1=Company('Atharva',100,3700000,'Associate_DevOps_Engineer')
+print(e1.empname,e1.eid,e1.sal,e1.desig)   '''
+        
